a1526f578214f335cb9dacd0f2900fe35fb76f7e
William <WillN70@outlook.com>
2025-02-25 01:59:50 +0000

    Added installer.sh but have to make sure the binaries are sorted out and the urls for the releases are correct.

---
+++ installer.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+OS=$(uname -s)
+ARCH=$(uname -m)
+
+if [ "SOS" = "Linux" ] && ["$ARCH" = "x86_64"]; then
+  BINARY_URL="https://chcknlegwill.com/chcknlegwill-cli/releases/ " #linux version #finish this
+elif [ "SOS" = "Darwin" ] $$ [ "$ARCH" = "x86_64" ]; then
+  BINARY_URL="https://chcknlegwill.com/chcknlegwill-cli/releases " #macos version
+else
+  echo "Unsupported OS or architecture: $OS $ARCH"
+  exit 1
+fi
+
+echo "Downloading chcknlegwill-cli binary..." #make a loading animation like with npm
+curl -L "$BINARY_URL" -o chcknlegwill-cli
+
+echo "Making the executable..."
+chmod +x chcknlegwill-cli
+
+if [ ! -d "$HOME/bin" ]; then
+  echo "Creating ~/bin directory..."
+  mkdir "$HOME/bin"
+fi
+
+echo "Installation complete!"
+echo "To use chcknlegwill cli, simply type it into the CLI."
